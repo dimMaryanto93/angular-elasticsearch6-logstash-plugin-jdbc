eb0ac52e2c1803abdf3e23dffdf65d7119e963a1
dimmaryanto93 <software.dimas_m93@icloud.com>
2019-02-10 18:25:20 +0700

    enabled search fields

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,6 +13,13 @@ export class AppComponent implements OnInit, AfterViewInit {
   title = 'Advance searching with elasticsearch';
   searchBox: FormGroup;
 
+  searchItems: any[] = [];
+  searchConfig: any = {
+    'placeholder': 'Search name of test script',
+    'sourceField': ['label']
+  };
+
+
   @ViewChild(DataTableDirective)
   dtElement: DataTableDirective;
   dtOptions: DataTables.Settings = {};
@@ -22,6 +29,42 @@ export class AppComponent implements OnInit, AfterViewInit {
     private _elasticsearch: ElasticsearchService,
     private _formBuilder: FormBuilder) {
   }
+
+  setFieldsSearchChanaged(item: string) {
+    this.searchBox.patchValue({
+      'search': item
+    });
+  }
+
+  onSelect(item: any) {
+    this.setFieldsSearchChanaged(item.label);
+  }
+
+  onChangeTextFieldSearch(val: string) {
+    console.log(val);
+    this.searchItems = [];
+    this._elasticsearch.searchFields(val)
+      .then(
+        response => {
+          if (response.hits) {
+            if (response.hits.hits) {
+              const list = response.hits.hits;
+              for (const source of list) {
+                const data = {
+                  label: `${source._source.test_filename}`,
+                  source: source._source
+                };
+                this.searchItems.push(data);
+              }
+            }
+          }
+        }
+      )
+      .catch(error => {
+        console.error('can not get data from elasticsearch!');
+      });
+  }
+
 
   ngOnInit(): void {
     this._elasticsearch.ping();
@@ -38,6 +81,7 @@ export class AppComponent implements OnInit, AfterViewInit {
       processing: true,
       ajax: (dataTablesParameters: any, callback) => {
         const data = that.searchBox.value;
+        console.log(data);
         that._elasticsearch.searchDatatables(
           data.search,
           dataTablesParameters.length,
